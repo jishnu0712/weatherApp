8263be69f12d9b8f9effac83c54ec45230ab8bd0
Jishnu <jishnu.dev.roy@gmail.com>
2022-04-23 17:32:08 +0530

    removed unwanted dependencies

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-const { on } = require("events");
-const { response } = require("express");
+
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
@@ -18,9 +17,11 @@ app.get("/", (req, res) => {
 
 app.post("/", function (req, res) {
 
-    console.log(req.body.cityName);
+    var cityName = req.body.cityName
+    var apiKey = "323eecb3b884f86eae937878ae160d27";
+    var unit = "metric"
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?appid=323eecb3b884f86eae937878ae160d27&q=malda&unit=metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?appid=" + apiKey + "&q=" + cityName + "&unit=" + unit + "";
 
     https.get(url, function (response) {
 
@@ -29,7 +30,7 @@ app.post("/", function (req, res) {
             // console.log(t.weather[0].description);
             const icon = t.weather[0].icon;
 
-            res.write("<h1>weather in Malda is " + t.weather[0].description + "<h1>");
+            res.write("<h1>weather in " + cityName + " is " + t.weather[0].description + "<h1>");
             res.write("<img src='http://openweathermap.org/img/wn/" + icon + "@2x.png'>");
             res.send();
         })
