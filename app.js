02f105b8ab8d67c4e8d1a5ec7647082429d37453
Jishnu <jishnu.dev.roy@gmail.com>
2022-04-23 17:57:58 +0530

    added git ignore

--- app.js
+++ app.js
@@ -17,34 +17,34 @@ app.get("/", (req, res) => {
 
 app.post("/", function (req, res) {
 
-    var cityName = req.body.cityName
-    var apiKey = "323eecb3b884f86eae937878ae160d27";
-    var unit = "metric"
+    let cityName = req.body.cityName;
+    const apiKey = "323eecb3b884f86eae937878ae160d27";
+    const unit = "metric";
 
     const url = "https://api.openweathermap.org/data/2.5/weather?appid=" + apiKey + "&q=" + cityName + "&unit=" + unit + "";
 
     https.get(url, function (response) {
 
         response.on("data", (data) => {
-            var t = JSON.parse(data);
-            // console.log(t.weather[0].description);
+            let t = JSON.parse(data);
+            console.log(t.weather[0].description);
             const icon = t.weather[0].icon;
 
             res.write("<h1>weather in " + cityName + " is " + t.weather[0].description + "<h1>");
             res.write("<img src='http://openweathermap.org/img/wn/" + icon + "@2x.png'>");
             res.send();
-        })
+        });
 
         console.log(response.statusCode);
     })
 
 
     console.log("post req received");
-})
+});
 
 
 
 
 app.listen(3000, function () {
     console.log("server is running.");
-})
+});
